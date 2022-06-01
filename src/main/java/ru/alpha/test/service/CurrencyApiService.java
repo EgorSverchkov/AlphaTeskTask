698ff9481d616a1c02fdc74b167b1eec0c5a0f67
Egor Sverchkov <sverch322@gmail.com>
2022-06-01 17:36:27 +0300

    create currencyApiService and fix CurrencyApiService

--- src/main/java/ru/alpha/test/service/CurrencyApiService.java
+++ src/main/java/ru/alpha/test/service/CurrencyApiService.java
@@ -1,17 +1,14 @@
 package ru.alpha.test.service;
 
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.alpha.test.model.dto.RequestCurrenctApi;
+import ru.alpha.test.model.dto.ResponseCurrencyApi;
 
-@FeignClient(name = "${service.name}",url = "${service.base.url}")
+
+@FeignClient(name = "${currency-service.name}", url = "${currency-service.base.url}")
 public interface CurrencyApiService {
     @GetMapping("/historical/{date}.json?app_id=3c76b3ac8e774c51b1f6cbac021c1bf5")
-    ResponseEntity<String> getCurrency(@PathVariable("date") String date);
+    ResponseCurrencyApi getCurrency(@PathVariable("date") String date);
 }
---
+++ src/main/java/ru/alpha/test/service/GifApiService.java
@@ -0,0 +1,17 @@
+package ru.alpha.test.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name = "${gif-service.name}", url = "${gif-service.base.url}")
+public interface GifApiService {
+
+    @GetMapping("/random?api_key=f0o1Mb2RD9YBtOzWtt7ZYi4Fvk9t95oQ&tag={tag}")
+    ResponseEntity<Map> getGif(@PathVariable("tag") String tag);
+
+}
